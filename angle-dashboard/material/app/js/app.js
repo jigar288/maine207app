9ab6067d110742df02619e874fb79542b26e6d38
Jigar Patel <jigar.moyo@gmail.com>
2017-12-20 23:27:17 -0600

    Update app.js

--- angle-dashboard/material/app/js/app.js
+++ angle-dashboard/material/app/js/app.js
@@ -1,13 +1,3 @@
-/*!
- * 
- * Angle - Bootstrap Admin App + AngularJS Material
- * 
- * Version: 3.5.4
- * Author: @themicon_co
- * Website: http://themicon.co
- * License: https://wrapbootstrap.com/help/licenses
- * 
- */
 
 // APP START
 // ----------------------------------- 
